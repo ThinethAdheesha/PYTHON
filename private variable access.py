1ad057a5a8ce0cd91b59fe70d3b0af4a90e04960
ThinethAdheesha <rataadeesha@gmail.com>
2025-01-28 21:45:29 +0530

    Create private variable access.py

---
+++ private variable access.py
@@ -0,0 +1,6 @@
+class python:
+ __x=10  #__x=it means about a private  varibale  we can  access with using a method othrwise we can't do it. 
+ def private(self):
+     return self.__x
+myobj=python( )
+print(myobj.private()) 
