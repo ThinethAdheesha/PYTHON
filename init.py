7d7bb6adb52b1218e0f31b68cb847ef453d85312
ThinethAdheesha <rataadeesha@gmail.com>
2025-01-30 10:59:32 +0530

    initialize

---
+++ init.py
@@ -0,0 +1,9 @@
+class  initialize :
+       def __init__(self,name,age):
+              self.name=name
+              self.age=age
+
+ 
+oob1=initialize("Thineth",21)
+print(oob1.name)
+print(oob1.age)
